bc964f0cf57d521fb4aaff8295d04ae9bdddb0e4
grusnny <37426267+grusnny@users.noreply.github.com>
2020-12-04 15:14:32 -0500

    Create method create table dynamodb Products
    Metodo que permite crear una tabla en DynamoDb con el nombre de Products si esta no existe.

--- blackstore-firebase-api/Configuration/DynamoDbConfiguration.cs
+++ blackstore-firebase-api/Configuration/DynamoDbConfiguration.cs
@@ -33,5 +33,55 @@ namespace blackstore_firebase_api.Configuration
                 client = new AmazonDynamoDBClient(credentials, RegionEndpoint.USEast1);
             }
 
+        public static DynamoDbConfiguration Instance
+        {
+            get
+            {
+                return _instance;
+            }
         }
+        public async Task<String> createTable()
+        {
+
+            Console.WriteLine("Verify table => " + tableName);
+            var tableResponse = await client.ListTablesAsync();
+            Console.WriteLine(tableResponse.TableNames);
+            if (!tableResponse.TableNames.Contains(tableName))
+            {
+                Console.WriteLine("Table not found, creating table => " + tableName);
+                await client.CreateTableAsync(new CreateTableRequest
+                {
+                    TableName = tableName,
+                    ProvisionedThroughput = new ProvisionedThroughput
+                    {
+                        ReadCapacityUnits = 3,
+                        WriteCapacityUnits = 1
+                    },
+                    KeySchema = new List<KeySchemaElement>
+                    {
+                        new KeySchemaElement
+                        {
+                            AttributeName = hashKey,
+                            KeyType = KeyType.HASH
+                        }
+                    },
+                    AttributeDefinitions = new List<AttributeDefinition>
+                    {
+                        new AttributeDefinition { AttributeName = hashKey, AttributeType=ScalarAttributeType.S }
+                    }
+                });
+
+                bool isTableAvailable = false;
+                while (!isTableAvailable)
+                {
+                    Console.WriteLine("Waiting for table to be active...");
+                    Thread.Sleep(5000);
+                    var tableStatus = await client.DescribeTableAsync(tableName);
+                    isTableAvailable = tableStatus.Table.TableStatus == "ACTIVE";
+                }
+            }
+            return "The table already exists!";
+
+        }
+    }
 }
