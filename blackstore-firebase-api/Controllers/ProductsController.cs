0377ad5bab7eeb33416b960ce9c8febc0bf46d97
Ocrezzu <davidjrf95@gmail.com>
2020-12-04 15:02:13 -0500

    Create ProductsController.cs
    Se crea el controller para los productos

---
+++ blackstore-firebase-api/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Cors;
+
+namespace blackstore_firebase_api.Controllers
+{
+    [EnableCors("MyPolicy")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+       
+
+      
+
+    }
+}
