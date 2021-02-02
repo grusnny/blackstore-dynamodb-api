b67ce25524b828fd66b9d1dc4102db04c1b12773
Ocrezzu <davidjrf95@gmail.com>
2021-02-02 17:54:16 -0500

    Pruebas

---
+++ MisTests/Fixture/ApiBlacksFixxture.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alba;
+
+
+namespace MisTests
+{
+    public class ApiBlacksFixxture : IDisposable
+    {
+        public readonly SystemUnderTest systemUnderTest;
+        public ApiBlacksFixxture()
+        {
+            systemUnderTest = SystemUnderTest.ForStartup<blackstore_firebase_api.Startup>();
+        }
+        public void Dispose()
+        {
+            systemUnderTest?.Dispose();
+        }
+    }
+}
---
+++ MisTests/TestClientProvider.cs
@@ -0,0 +1,28 @@
+﻿using blackstore_firebase_api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisTests
+{
+    public class TestClientProvider : IDisposable
+    {
+        public HttpClient Client { get; private set; }
+        public TestClientProvider()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            
+            Client?.Dispose();
+        }
+    }
+}
