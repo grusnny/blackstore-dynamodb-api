3890751b71a32031b5555971b1ba201b3eb1c0aa
grusnny <37426267+grusnny@users.noreply.github.com>
2021-02-02 10:08:45 -0500

    Corse Fix

--- blackstore-firebase-api/Startup.cs
+++ blackstore-firebase-api/Startup.cs
@@ -26,6 +26,12 @@ namespace blackstore_firebase_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +47,8 @@ namespace blackstore_firebase_api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("MyPolicy");
 
             app.UseEndpoints(endpoints =>
             {
