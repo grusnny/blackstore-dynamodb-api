e41984b7c70460a7daed5066f5220982eac28e8a
Ocrezzu <davidjrf95@gmail.com>
2021-02-02 15:12:01 -0500

    Pruebas
    Inicio de pruebas

---
+++ MisTests/TestProducts.cs
@@ -0,0 +1,39 @@
+﻿using Alba;
+using blackstore_firebase_api.Entity;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MisTests
+{
+    public class TestProducts : IClassFixture<ApiBlacksFixxture>
+    {
+        private readonly SystemUnderTest _system;
+        public TestProducts(ApiBlacksFixxture app)
+        {
+            _system = app.systemUnderTest;
+        }
+
+
+        [Fact]
+        public async Task Very_GetAll()
+        {
+            var results = await _system.GetAsJson<IList<Product>>("/api");
+            results.Count.Should().Be(27);
+        }
+        [Fact]
+        public async Task TestGetAll_Ok()
+        {
+            var client = new TestClientProvider().Client;
+            var results = await client.GetAsync("/api");
+            results.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, results.StatusCode);
+
+        }
+    }
+}
